8c652da2173fffab4b913679eb7cce69b6eb49b4
Augustin Husson <husson.augustin@gmail.com>
2017-10-08 13:28:17 +0200

    add remove all indices after execute an unit test add assert for the first test

--- tests/Nexucis/Elasticsearch/Helper/Nodowntime/Tests/IndexHelperTest.php
+++ tests/Nexucis/Elasticsearch/Helper/Nodowntime/Tests/IndexHelperTest.php
@@ -2,6 +2,7 @@
 
 namespace Nexucis\Elasticsearch\Helper\Nodowntime\Tests;
 
+use Elasticsearch\Client;
 use Elasticsearch\ClientBuilder;
 use Nexucis\Elasticsearch\Helper\Nodowntime\IndexHelper;
 use Nexucis\Elasticsearch\Helper\Nodowntime\IndexHelperInterface;
@@ -15,18 +16,35 @@ class IndexHelperTest extends TestCase
     private static $HELPER;
 
     /**
+     * @var $client Client
+     */
+    private static $client;
+
+    /**
      * initialize elasticsearch client and index Helper
      */
     public static function setUpBeforeClass()
     {
-        $client = ClientBuilder::create()->setHosts([$_SERVER['ES_TEST_HOST']])->build();
+        self::$client = ClientBuilder::create()->setHosts([$_SERVER['ES_TEST_HOST']])->build();
         self::$HELPER = new IndexHelper();
-        self::$HELPER->setClient($client);
+        self::$HELPER->setClient(self::$client);
+    }
+
+    public function tearDown()
+    {
+        // remove all previously indices created by test or by the before setup
+        $param = [
+            'index' => '_all'
+        ];
+        self::$client->indices()->delete($param);
     }
 
     public function testCreateIndex()
     {
-        self::$HELPER->createIndex('myindextest');
+        $alias = 'myindextest';
+        self::$HELPER->createIndex($alias);
+        $this->assertTrue(self::$HELPER->existsIndex($alias));
+        $this->assertTrue(self::$HELPER->existsIndex($alias . '_v1'));
     }
 
 }
