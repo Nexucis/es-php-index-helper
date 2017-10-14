7f3a0782fc59dde491d8943dd4bc438713a78b81
Augustin Husson <husson.augustin@gmail.com>
2017-10-14 13:59:10 +0200

    improve waiting es cluster available

--- .circleci/wait_es_up.sh
+++ .circleci/wait_es_up.sh
@@ -2,12 +2,14 @@
 
 loop=0
 response=7 # node not available
+wait=1
 
 while [ $response != 0 ] && [ $loop != 5 ]; do
     curl -s -XHEAD http://localhost:9200
     response=$?
     loop=$(expr $loop + 1)
-    sleep 1
+    wait=$(expr $wait * 2)
+    sleep $wait
 done
 
 if [ $loop == 5 ]; then
